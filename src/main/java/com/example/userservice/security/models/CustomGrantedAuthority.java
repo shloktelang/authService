9ff77b47bba1406e80e307df486facb9553a29bb
Shlok Telang <shloktelang@Shloks-MacBook-Air.local>
2023-11-28 14:03:08 +0530

    oauth2 with db complete

--- src/main/java/com/example/userservice/security/models/CustomGrantedAuthority.java
+++ src/main/java/com/example/userservice/security/models/CustomGrantedAuthority.java
@@ -1,17 +1,23 @@
 package com.example.userservice.security.models;
 
 import com.example.userservice.models.Role;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
+@JsonDeserialize
+@NoArgsConstructor
 public class CustomGrantedAuthority implements GrantedAuthority {
-    private Role role;
+    //    private Role role;
+    private String authority;
 
     public CustomGrantedAuthority(Role role) {
-        this.role = role;
+        this.authority = role.getName();
     }
+
 
     @Override
     public String getAuthority() {
-        return role.getName();
+        return this.authority;
     }
 }
--- src/main/java/com/example/userservice/security/models/CustomUserDetails.java
+++ src/main/java/com/example/userservice/security/models/CustomUserDetails.java
@@ -2,6 +2,9 @@ package com.example.userservice.security.models;
 
 import com.example.userservice.models.Role;
 import com.example.userservice.models.User;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -9,48 +12,93 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@JsonDeserialize
+@NoArgsConstructor
 public class CustomUserDetails implements UserDetails {
-    private User user;
+//    private User user;
 
-    public CustomUserDetails(User user){
-        this.user = user;
+    private List<GrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    private Long userId;
+
+
+
+    public CustomUserDetails(User user) {
+
+//        this.user = user;
+        authorities = new ArrayList<>();
+
+        for (Role role: user.getRoles()) {
+            authorities.add(new CustomGrantedAuthority(role));
+        }
+
+        this.password = user.getPassword();
+        this.username = user.getEmail();
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+        this.userId = user.getId();
     }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<CustomGrantedAuthority> customGrantedAuthorities = new ArrayList<>();
-        for(Role role : user.getRoles()){
-            customGrantedAuthorities.add(new CustomGrantedAuthority(role));
-        }
-        return customGrantedAuthorities;
+//        List<CustomGrantedAuthority> customGrantedAuthorities = new ArrayList<>();
+//
+//        for (Role role: user.getRoles()) {
+//            customGrantedAuthorities.add(new CustomGrantedAuthority(role));
+//        }
+//
+//        return customGrantedAuthorities;
+        return this.authorities;
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword(); // this is the password that is stored in the database
+//        return user.getPassword();
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return user.getEmail(); // this is the email that is stored in the database
+//        return user.getEmail();
+        return this.username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+//        return true;
+        return this.accountNonExpired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+//        return true;
+        return this.accountNonLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+//        return true; // if (Date.now() - lastPasswordUpdateDate > 90) {return false} return true;
+        return this.credentialsNonExpired;
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+//        return true;
+        return this.enabled;
     }
 }
