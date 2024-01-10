64e047d6bb4869a6f85f4e12ac4789d23cc5f9b8
Shlok Telang <shloktelang@Shloks-MacBook-Air.local>
2024-01-10 20:38:56 +0530

    Refactored code

--- src/test/java/com/example/userservice/InsertRegisteredClientTest.java
+++ src/test/java/com/example/userservice/InsertRegisteredClientTest.java
@@ -1,42 +1,42 @@
-//package com.example.userservice;
-//
-//import com.example.userservice.security.repositories.JpaRegisteredClientRepository;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.oauth2.core.AuthorizationGrantType;
-//import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-//import org.springframework.security.oauth2.core.oidc.OidcScopes;
-//import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-//import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-//
-//import java.util.UUID;
-//
+package com.example.userservice;
+
+import com.example.userservice.security.repositories.JpaRegisteredClientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
+
 //@SpringBootTest
-//public class InsertRegisteredClientTest {
+public class InsertRegisteredClientTest {
 //    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
+    private PasswordEncoder passwordEncoder;
+
 //    @Autowired
-//    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
-//
+    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
+
 //    @Test
-////    @Commit
-//    public void insertNewClientToDb() {
-//        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-//                .clientId("postman")
-//                .clientSecret(passwordEncoder.encode("postmanpassword"))
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-//                .redirectUri("https://oauth.pstmn.io/v1/callback")
-//                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-//                .scope(OidcScopes.OPENID)
-//                .scope(OidcScopes.PROFILE)
-//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-//                .build();
-//
-//        jpaRegisteredClientRepository.save(oidcClient);
-//    }
-//}
+//    @Commit
+    public void insertNewClientToDb() {
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("postman")
+                .clientSecret(passwordEncoder.encode("postmanpassword"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
+                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        jpaRegisteredClientRepository.save(oidcClient);
+    }
+}
